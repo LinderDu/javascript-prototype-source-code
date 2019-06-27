6284f3670a934cd6122b19becbca1c00298bb4b5
haolin.du <haolin.du@56qq.com>
2019-06-27 15:52:15 +0800

    add call、apply

---
+++ Apply/index.js
@@ -0,0 +1,29 @@
+/** 
+ * Apply 函数
+*/
+
+
+Function.prototype._apply = function () {
+  let result;
+  let [toThis, ...arges] = [...arguments];
+
+  if (!toThis) {
+    // 如果 onThis 为 null 或者 undefined
+    toThis = typeof window === 'undefined' ? global : window;
+  }
+
+  // this的指向当前函数  
+  //（理解下面这两步很重要，核心）
+  toThis.fun = this;
+  if (!arges) {
+    result = toThis.fun();
+  } else {
+    result = toThis.fun(arges);
+  }
+
+  
+  // toThis 没有func 属性，所以删除
+  delete toThis.fun;
+  return result;
+}
+
--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 本文主要分析JavaScript 个别函数内部的实现，一起脱掉它(她)的外衣，探索宇宙奥秘；
 
 * [New 函数背后的秘密](./New/index.js)
+* [Call 函数内部奥秘](./Call/index.js)
+* [Apply 函数内部奥秘](./Apply/index.js)
 * [Bind 函数内部奥秘](./Bind/index.js)
